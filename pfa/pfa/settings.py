d37c555ddbfb4edf97c56bc1b02c47594bcd870b
TomiGW <tomi.loqi@gmail.com>
2021-08-16 23:51:14 -0300

    Primera prueba: subir produccion

--- pfa/pfa/settings.py
+++ pfa/pfa/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 import os
+STATUC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATUC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS= (
     os.path.join(BASE_DIR, 'static'),
